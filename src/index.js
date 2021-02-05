b02eeac70e7834573238e73774055200ba02240d
leoportogtr86 <leoporto_studio@hotmail.com>
2021-02-05 13:38:43 -0300

    desafio resolvido

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ const { ApolloServer, gql } = require('apollo-server')
 
 const typeDefs = gql`
 
+
+
+    type Produto {
+
+        nome: String!
+        preco: Float!
+        desconto: Float
+        precoComDesconto: Float
+    }
+
+    type Carro {
+
+    marca: String
+    modelo: String
+    preco: Float
+    cor: String
+    novo: Boolean
+    
+    }
+
     type Pessoa {
 
         nome: String
@@ -9,17 +29,43 @@ const typeDefs = gql`
         profissao: String
     }
 
+
+
+
     type Query {
 
         ola: String
         nome: String
         pessoa: Pessoa
+        carro: Carro
+        produtoEmDestaque: Produto
     }
+
 
 
 `
 
 const resolvers = {
+
+    Produto: {
+
+        precoComDesconto(e) {
+
+            return e.preco - (e.preco * e.desconto / 100)
+        }
+
+
+    },
+
+    Carro: {
+
+        novo(e) {
+
+            return e.unicoDono
+
+
+        }
+    },
 
     Query: {
 
@@ -40,6 +86,28 @@ const resolvers = {
                 nome: "Kiko Loureiro",
                 idade: 45,
                 profissao: "músico"
+            }
+        },
+
+        carro() {
+
+            return {
+
+                marca: 'Land Rover',
+                modelo: 'Range Rover Evoque',
+                preco: 350000,
+                cor: 'branco',
+                unicoDono: false
+            }
+        },
+
+        produtoEmDestaque() {
+
+            return {
+
+                nome: 'Mac Book Air',
+                preco: 15000,
+                desconto: 10
             }
         }
     }
